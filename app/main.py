35ab447dbbeb90d78b5b6fa344905ba5919d6c6d
olegmir59 <udom1@ya.ru>
2025-01-19 01:25:54 +0700

    Modify files

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
-
-from app import routers
+from app.routers import task
+from app.routers import user
+import uvicorn
 
 app = FastAPI()
 
-app.include_routers([routers])
+@app.get("/")
+async def welcome():
+    return {"message":  "Welcome to Taskmanager"}
 
-app.add_api_route('/welcome', include_in_schema=False)({"message": "Welcome to Taskmanager"})
 
+app.include_router(user.router)
+app.include_router(task.router)
 
 if __name__ == '__main__':
-    print('PyCharm')
+    uvicorn.run(app, host='127.0.0.1', port=8000)
